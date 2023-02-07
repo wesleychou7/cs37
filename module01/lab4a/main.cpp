3bc232d5485b74039ad6611ca306f656cbe9327f
Wesley Chou <wesleychou9851@gmail.com>
2023-02-07 14:42:45 -0800

    completed extra credit

--- module01/lab4a/main.cpp
+++ module01/lab4a/main.cpp
@@ -4,33 +4,64 @@ using namespace std;
 int main(){
 
     int input;
-
     cout << "Enter a 5-digit integer that contain only 0s and 1s: ";
     cin >> input;
 
-    
     int check_input = input;
-    int i = 0;
+    int digit_count = 0;
     int digit;
+
+    // check input
+    while (check_input){
+
+        digit = check_input % 10;
+        check_input /= 10;
+        digit_count++;
+
+        if (digit != 0 && digit != 1){
+            cout << "Error: Invalid input.\n" << endl;
+            // prompt new input
+            cout << "Enter a 5-digit integer that contain only 0s and 1s: ";
+            cin >> input;
+
+            // reset values
+            check_input = input;
+            digit_count = 0;
+            digit = 0;
+        }
+
+        // check if input has length 5
+        while (digit_count != 5){
+
+            cout << "Error: Invalid input.\n" << endl;
+            // prompt new input
+            cout << "Enter a 5-digit integer that contain only 0s and 1s: ";
+            cin >> input;
+
+            digit_count = 0;
+            check_input = input;
+
+            while (check_input){
+                check_input /= 10;
+                digit_count++ ;
+            }
+        }
+
+    }
+
+
+    // reset and initialize values
+    check_input = input;
+    digit = 0;
+    int i = 0;
     int decimal = 0;
 
-    while (i  < 5){
+    // when input is valid, calculate decimal value
+    while (i < 5){
 
         // get right-most digit
         digit = check_input % 10;
-
-        // check if input is 5 digits
-        // will finish this next time
-
         check_input /= 10;
-
-        // check if user input contains only 0s and 1s
-        if (digit != 0 && digit != 1){
-            cout << "Error: invalid input. Enter only 0s or 1s.\n\n" << endl;
-            cout << "Enter a 5-digit integer that contain only 0s and 1s: ";
-            cin >> input;
-            i = 0;
-        }
         
         // get the 2^exponent value ; e.g., 2^3 = 8
         int j = 0;
@@ -40,12 +71,12 @@ int main(){
             j++;
         }
 
-        // update decimal value
+        // update values
         decimal += digit * multiply;
-
         i++;
     }
 
+    
     cout << "The decimal value is: " << decimal << endl;
 
     return 0;
