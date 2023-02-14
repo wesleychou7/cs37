e4a60f6c96364227f496a60c645e7450291d655f
Wesley Chou <wesleychou9851@gmail.com>
2023-02-14 15:57:21 -0800

    dollar amount part 1 implementation

--- assignments/lab5a/DollarAmount.h
+++ assignments/lab5a/DollarAmount.h
@@ -4,41 +4,73 @@
 #include <cmath> 
 
 class DollarAmount {
-public:
-   // initialize amount from an int64_t value
-   explicit DollarAmount(int64_t value) : amount{value} { }
+   public:
+      // initialize dollars & cents from an int64_t value
+      int dollars;
+      int cents;
+      explicit DollarAmount(int64_t value){
+         dollars = value / 100;
+         cents = value % 100;
+      }
 
-   // add right's amount to this object's amount
-   void add(DollarAmount right) {
-      // can access private data of other objects of the same class
-      amount += right.amount; 
-   }
+      // add right's amount to this object's amount
+      void add(DollarAmount right) {
+         // can access private data of other objects of the same class
+         dollars += right.dollars;
+         
+         int add_cents = cents + right.cents;
 
-   // subtract right's amount from this object's amount
-   void subtract(DollarAmount right) {
-      // can access private data of other objects of the same class
-      amount -= right.amount;
-   }
+         if (add_cents >= 100){
+            dollars += 1;
+            add_cents -= 100;
+            cents = add_cents;
+         }
+         else {
+            cents += right.cents;
+         }
+         
+      }
 
-   // uses integer arithmetic to calculate interest amount, 
-   // then calls add with the interest amount
-   void addInterest(int rate, int divisor) {
-      // create DollarAmount representing the interest
-      DollarAmount interest{
-         (amount * rate + divisor / 2) / divisor
-      };
+      // subtract right's amount from this object's amount
+      void subtract(DollarAmount right) {
+         // can access private data of other objects of the same class
+         dollars -= right.dollars;
+         
+         int sub_cents = cents - right.cents;
 
-      add(interest); // add interest to this object's amount
-   }
+         if (sub_cents < 0){
+            sub_cents = 100 + sub_cents;
+            cents = sub_cents;
+            dollars -= 1;
+         }
+         else{
+            cents = 100 - sub_cents;
+            dollars += 1;
+         }
+      }
 
-   // return a string representation of a DollarAmount object
-   std::string toString() const {
-      std::string dollars{std::to_string(amount / 100)};
-      std::string cents{std::to_string(std::abs(amount % 100))};
-      return dollars + "." + (cents.size() == 1 ? "0" : "") + cents;
-   }
-private:
-   int64_t amount{0}; // dollar amount in pennies
+      // uses integer arithmetic to calculate interest amount, 
+      // then calls add with the interest amount
+      void addInterest(int rate, int divisor) {
+         // create DollarAmount representing the interest
+
+         DollarAmount interest{((dollars * 100 + cents) * rate + divisor / 2) / divisor};
+
+         add(interest); // add interest to this object's amount
+      }
+
+      // return a string representation of a DollarAmount object
+      std::string toString(){
+         if (std::to_string(cents).length() > 1){
+            return std::to_string(dollars) + "." + std::to_string(cents);
+         }
+         else{
+            return std::to_string(dollars) + ".0" + std::to_string(cents);
+         }
+      }
+
+   private:
+      int64_t amount = 0; // dollar amount in pennies
 };
 
 
--- assignments/lab5a/Interest.cpp
+++ assignments/lab5a/Interest.cpp
@@ -10,7 +10,10 @@ int main() {
    DollarAmount d1{12345}; // $123.45
    DollarAmount d2{1576}; // $15.76
 
-   cout << "After adding d2 (" << d2.toString() << ") into d1 ("
+   // cout << d1.dollars << "." << d1.cents << endl;
+   // cout << d2.dollars << "." << d2.cents << endl;
+
+   cout << "After adding d2 (" << d2.toString() << ") into d1 (" 
       << d1.toString() << "), d1 = ";
    d1.add(d2); // modifies object d1
    cout << d1.toString() << "\n";
@@ -49,18 +52,3 @@ int main() {
       cout << setw(4) << year << setw(20) << balance.toString() << endl;
    }
 }
-
-/**************************************************************************
-* (C) Copyright 1992-2017 by Deitel & Associates, Inc. and               *
-* Pearson Education, Inc. All Rights Reserved.                           *
-*                                                                        *
-* DISCLAIMER: The authors and publisher of this book have used their     *
-* best efforts in preparing the book. These efforts include the          *
-* development, research, and testing of the theories and programs        *
-* to determine their effectiveness. The authors and publisher make       *
-* no warranty of any kind, expressed or implied, with regard to these    *
-* programs or to the documentation contained in these books. The authors *
-* and publisher shall not be liable in any event for incidental or       *
-* consequential damages in connection with, or arising out of, the       *
-* furnishing, performance, or use of these programs.                     *
-*************************************************************************/
