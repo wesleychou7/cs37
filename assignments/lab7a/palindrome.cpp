e84ff29393b179e8f0a1cff3f0ec948db0c1c2f0
Wesley Chou <wesleychou9851@gmail.com>
2023-03-02 09:08:11 -0800

    initial commit

---
+++ assignments/lab7a/palindrome.cpp
@@ -0,0 +1,28 @@
+#include <iostream> 
+#include <string>
+using namespace std;
+
+bool testPalindrome(string, size_t, size_t);
+
+int main() {
+   string input;
+
+   // get sentence to test from user
+   cout << "Enter a sentence:\n";
+   getline(cin, input); //gets the whole line of text
+
+   // Print whether or not sentence is palindrome
+   int lastElement = input.length() - 1;
+   if (testPalindrome(input, 0, lastElement)) {
+      cout << '\"' << input << "\" is a palindrome" << endl;
+   }
+   else {
+      cout << '\"' << input << "\" is not a palindrome" << endl;
+   }
+}
+
+bool testPalindrome(string palindrome, size_t left, size_t right) {
+  //
+  // TO DO
+  //
+}
