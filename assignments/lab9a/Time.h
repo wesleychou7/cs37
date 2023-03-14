61671c70254845903d2f491de62288a0b5bf2349
Wesley Chou <wesleychou9851@gmail.com>
2023-03-14 09:48:59 -0700

    initial commit

---
+++ assignments/lab9a/Time.h
@@ -0,0 +1,34 @@
+// Time class containing a constructor with default arguments.
+// Member functions defined in Time.cpp.
+#include <string>
+
+// prevent multiple inclusions of header
+#ifndef TIME_H
+#define TIME_H
+
+// Time class definition
+class Time {
+public:
+   explicit Time(int = 0, int = 0, int = 0); // default constructor
+
+   // set functions
+   void setTime(int, int, int); // set hour, minute, second
+   void setHour(int); // set hour (after validation)
+   void setMinute(int); // set minute (after validation)
+   void setSecond(int); // set second (after validation)
+
+   // get functions
+   unsigned int getHour() const; // return hour
+   unsigned int getMinute() const; // return minute
+   unsigned int getSecond() const; // return second
+
+   std::string toUniversalString() const; // 24-hour time format string
+   std::string toStandardString() const; // 12-hour time format string
+   void tick(); // increment one second
+private:
+   unsigned int hour{0}; // 0 - 23 (24-hour clock format)
+   unsigned int minute{0}; // 0 - 59
+   unsigned int second{0}; // 0 - 59
+};
+
+#endif
---
+++ assignments/lab9a/TimeTester.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+#include "Time.h" // include definition of class Time
+using namespace std;
+
+const int MAX_TICKS{30}; //
+
+int main() {
+   Time t{23, 59, 57}; // instantiate object t of class Time
+
+   // output Time object t's values
+   for (int ticks{1}; ticks < MAX_TICKS; ++ticks) {
+
+     //
+     // TO DO - MAX_TICKS can be changed to create a 
+     //         longer loop
+     //
+     
+   } 
+}
