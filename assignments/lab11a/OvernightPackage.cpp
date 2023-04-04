48bd1459f64af135e4dfe65b71ff1f829129d843
Wesley Chou <wesleychou9851@gmail.com>
2023-04-04 10:08:53 -0700

    compeleted lab11a

--- assignments/lab11a/OvernightPackage.cpp
+++ assignments/lab11a/OvernightPackage.cpp
@@ -1,31 +1,39 @@
 // Member-function definitions of class OvernightPackage.
 #include <stdexcept>
+#include "TwoDayPackage.h"
 #include "OvernightPackage.h" // OvernightPackage class definition
 
+
 // constructor
-OvernightPackage::OvernightPackage() : {
-   //
-   // TO DO
-   //
+OvernightPackage::OvernightPackage(const string &sName, 
+   const string& sAddress, const string& sCity, const string& sState, 
+   int sZIP, const string& rName, const string& rAddress, 
+   const string& rCity, const string& rState, int rZIP, 
+   double w, double cost, double fee) 
+   
+   : Package(sName, sAddress, sCity, sState, sZIP, 
+      rName, rAddress, rCity, rState, rZIP, w, cost) {
+   setOvernightFeePerOunce(fee);
 } 
 
 // set overnight fee
-void OvernightPackage::setOvernightFeePerOunce() {
-   //
-   // TO DO
-   //
+void OvernightPackage::setOvernightFeePerOunce(double fee) {
+   if (fee >= 0.0) {
+      overnightFeePerOunce = fee;
+   }
+   else {
+      throw invalid_argument("Overnight fee must be >= 0.0");
+   }
 } 
 
 // return overnight fee
 double OvernightPackage::getOvernightFeePerOunce() const {
-   //
-   // TO DO
-   //
+   return overnightFeePerOunce;
 } 
 
 // calculate shipping cost for package
 double OvernightPackage::calculateCost() const {
-   //
-   // TO DO
-   //
+
+   return Package::getWeight() * (Package::getCostPerOunce() + getOvernightFeePerOunce());
+   
 } 
