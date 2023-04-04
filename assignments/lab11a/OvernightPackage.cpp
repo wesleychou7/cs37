59913ae5497c3932253e9fe78e7dc16482b3582e
Wesley Chou <wesleychou9851@gmail.com>
2023-04-04 09:40:56 -0700

    initial commit

---
+++ assignments/lab11a/OvernightPackage.cpp
@@ -0,0 +1,31 @@
+// Member-function definitions of class OvernightPackage.
+#include <stdexcept>
+#include "OvernightPackage.h" // OvernightPackage class definition
+
+// constructor
+OvernightPackage::OvernightPackage() : {
+   //
+   // TO DO
+   //
+} 
+
+// set overnight fee
+void OvernightPackage::setOvernightFeePerOunce() {
+   //
+   // TO DO
+   //
+} 
+
+// return overnight fee
+double OvernightPackage::getOvernightFeePerOunce() const {
+   //
+   // TO DO
+   //
+} 
+
+// calculate shipping cost for package
+double OvernightPackage::calculateCost() const {
+   //
+   // TO DO
+   //
+} 
